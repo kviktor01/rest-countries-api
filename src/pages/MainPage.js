5594c29e0015bc460dbb7e63d129bf86c4119c9a
movvsv <movvsv@inf.elte.hu>
2022-09-19 11:24:17 +0200

    Error fixing

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -81,7 +81,7 @@ export default function MainPage() {
 				<select value={region} onChange={(e) => onRegionChange(e)}>
 					<option value="">Filter by region</option>
 					<option value="Africa">Africa</option>
-					<option value="America">America</option>
+					<option value="Americas">America</option>
 					<option value="Asia">Asia</option>
 					<option value="Europe">Europe</option>
 					<option value="Oceania">Oceania</option>
