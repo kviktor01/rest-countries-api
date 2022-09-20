b943ef096159ff8aa7c1fc4a7a0a0d1e92974f10
movvsv <movvsv@inf.elte.hu>
2022-09-20 09:38:35 +0200

    Add sort feature

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -16,7 +16,7 @@ import {
 
 export default function MainPage() {
 	const [getApiCountries] = useLazyGetCountriesQuery();
-	
+
 	const dispatch = useDispatch();
 	const countries = useSelector(getCountries);
 	const allCountry = useSelector(getAllCountry);
@@ -29,21 +29,28 @@ export default function MainPage() {
 					return substring.toLowerCase() === name;
 				});
 				console.log(countries2);
-				if(region !== "") {
-					countries2 = countries2.filter((country) => country.region === region);
+				if (region !== "") {
+					countries2 = countries2.filter(
+						(country) => country.region === region
+					);
 				}
+				countries2 = sortCountries(countries2);
 				dispatch(setCountries(countries2));
 				return;
 			}
 			if (region !== "") {
 				countries2 = allCountry.filter((country) => country.region === region);
+				countries2 = sortCountries(countries2);
 				dispatch(setCountries(countries2));
 				return;
 			}
-			
 		}
-		
-		dispatch(setCountries(allCountry));
+		let countries3=null;
+		if(allCountry){
+		countries3 = JSON.parse(JSON.stringify(allCountry));
+		countries3 = sortCountries(countries3);
+		}
+		dispatch(setCountries(countries3));
 	};
 	useEffect(() => {
 		if (!allCountry) {
@@ -51,34 +58,89 @@ export default function MainPage() {
 				.unwrap()
 				.then((data) => {
 					console.log(data);
-					dispatch(setAllCountry(data));
-					dispatch(setCountries(data));
+					let countries = JSON.parse(JSON.stringify(data));
+					countries = sortCountries(countries);
+					dispatch(setAllCountry(countries));
+					dispatch(setCountries(countries));
 				});
+
 			console.log("hello");
 		}
 	}, []);
-	
+	const sortCountries = (countries) => {
+		if (sortDirection === "ascending") {
+			switch (sortProp) {
+				case "name":
+					countries.sort((c1, c2) => {
+						//console.log(c1.name.common, c2.name.common);
+						return c1.name.common.localeCompare(c2.name.common);
+					});
 
+					return countries;
+				case "population":
+					console.log("hello from population")
+					countries.sort((c1, c2) => {
+						return (c1.population - c2.population);
+					});
+					return countries;
+				case "area":
+					countries.sort((c1, c2) => {
+						return (c1.area - c2.area );
+					});
+					return countries;
+				default:
+					return countries;
+			}
+		} else {
+			switch (sortProp) {
+				case "name":
+					countries.sort((c1, c2) => {
+						//console.log(c1.name.common, c2.name.common);
+						return c2.name.common.localeCompare(c1.name.common);
+					});
+
+					return countries;
+				case "population":
+					countries.sort((c1, c2) => {
+						return c2.population - c1.population;
+					});
+					return countries;
+				case "area":
+					countries.sort((c1, c2) => {
+						return c2.area - c1.area;
+					});
+					return countries;
+				default:
+					return countries;
+			}
+		}
+	};
 	const [name, setName] = useState("");
 	const [region, setRegion] = useState("");
-
+	const [sortProp, setSortProp] = useState("name");
+	const [sortDirection, setSortDirection] = useState("ascending");
 	useEffect(() => {
 		onSearch();
-	},[name, region]);
-	
-	const onRegionChange = (e) => {
-		setRegion(e.target.value);
-		
+	}, [name, region]);
+
+	useEffect(() => {
+		if (allCountry) {
+			let countries2 = JSON.parse(JSON.stringify(countries));
+			countries2 = sortCountries(countries2);
+			dispatch(setCountries(countries2));
+			console.log(sortProp);
+		}
+	}, [sortProp, sortDirection]);
+
+	const onChange = (e, fn) => {
+		fn(e.target.value);
 	};
-	const onNameSearch = (e) => {
-		setName(e.target.value);
-		
-	};
+
 	return (
 		<div className="container">
 			<div className="filter-container">
-				<Search value={name} onSearch={onNameSearch}></Search>{" "}
-				<select value={region} onChange={(e) => onRegionChange(e)}>
+				<Search value={name} onSearch={(e) => onChange(e, setName)}></Search>{" "}
+				<select value={region} onChange={(e) => onChange(e, setRegion)}>
 					<option value="">Filter by region</option>
 					<option value="Africa">Africa</option>
 					<option value="Americas">America</option>
@@ -86,6 +148,31 @@ export default function MainPage() {
 					<option value="Europe">Europe</option>
 					<option value="Oceania">Oceania</option>
 				</select>
+			</div>
+			<div className="sort-container">
+				<div>
+					<label htmlFor="sort">Sort by</label>
+					<select
+						name="sort"
+						value={sortProp}
+						onChange={(e) => onChange(e, setSortProp)}
+					>
+						<option value="name">Name</option>
+						<option value="population">Population</option>
+						<option value="area">Area</option>
+					</select>
+				</div>
+				<div>
+					<label htmlFor="direction">Sort direction</label>
+					<select
+						name="direction"
+						onChange={(e) => onChange(e, setSortDirection)}
+						value={sortDirection}
+					>
+						<option value="descending">Descending</option>
+						<option value="ascending">Ascending</option>
+					</select>
+				</div>
 			</div>
 			{countries ? (
 				<CountryCards countries={countries}></CountryCards>
